00ec31ce948e5a514c246fb111fd1b8eb2c6d917
Emily Cheng <echeng0123@gmail.com>
2023-08-03 15:27:51 -0400

    tab 2 puppydata with use state completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
+import { puppyList } from './data.js'
+import { useState } from 'react'
 
 function App() {
 
-
-  return (
-    <>
-      <div>
-
-      </div>
-    </>
-  )
+    const [puppies, setPuppies] = useState(puppyList);
+    console.log(puppies)
+    return (
+        <div className='App'>
+            {
+                puppies.map((puppy) => {
+                    return <p key={puppy.id}>{puppy.name}</p>
+                    })
+            }
+        </div>
+    )
 }
 
 export default App
